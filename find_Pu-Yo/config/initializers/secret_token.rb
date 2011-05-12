d11fd5af1a6eca24e62fb8294cc798abe92fea8b
Chia-Ling Lee <spiderman0103@gmail.com>
2011-05-12 10:52:38 +0800

    New a rails project.

---
+++ find_Pu-Yo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindPuYo::Application.config.secret_token = '5e39130d9cb49759a32d1f6290953df089db22698d34a684ea4c242fd147ab2a5bd4c947a437e99638b5fb7eafe99e0af3675b2a2811c8c677904072b180e767'

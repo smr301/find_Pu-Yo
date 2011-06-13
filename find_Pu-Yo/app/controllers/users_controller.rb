b6449f88d613748b0e09f243a0bb46bdb7d03707
Chia-Ling Lee <spiderman0103@gmail.com>
2011-06-13 17:08:24 +0800

    Finished level 2 friends of zerodie test: show zerodie's 2levels friends and count.

--- find_Pu-Yo/app/controllers/users_controller.rb
+++ find_Pu-Yo/app/controllers/users_controller.rb
@@ -7,52 +7,26 @@
 
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
+  def index
+    @user = User.find(1) #zerodie
+    @friends = @user.all_friends
+    @counter = count(@friends)
     #@user.uid = user_id2uid(@user.name)
     respond_to do |format|
       format.html
     end
   end
-
   
-  #POST /users
-  def create
-    @user = User.new(params[:user])
-    
-    respond_to do |format|
-      @user.uid = user_id2uid(@user.name)
-      
-      if @user.save
-        add_friends_to_database(@user)
-        format.html { redirect_to(@user, :notice => 'User was successfully created.' ) }
-      else
-        format.html { render :action => "new" }
+  def count(friends_l1)
+    counter = {}
+    counter.default = 0
+    friends_l1.each do |f|
+      friends_l2 = f.all_friends
+      friends_l2.each do |f2|
+        counter[f2.nick_name] = counter[f2.nick_name] + 1
       end
     end
-  end
-
-  
-  def user_id2uid(user_id) #zerodie return =>267660
-    public_profile_url = GET_PROFILE_URL + user_id
-    uri = URI.parse(public_profile_url)
-    res = Net::HTTP.get_response uri
-    data = res.body
-    public_profile = JSON.parse(data)
-    uid = public_profile["user_info"]["uid"] 
+    counter
   end
       
-  #GET /users/new
-  def new
-  
-    @user = User.new
-   
-    respond_to do |format|
-      format.html
-      #fomat.xml { render :xml => @user }
-    end
-  end
-
-  
-
 end
